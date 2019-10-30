f3d4b19d7b7e530187b0b2ea5ebb851461530e35
IT-Kubat <kubat88@list.ru>
2019-10-30 17:40:50 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int temperature = 35;
+	boolean isRainy = !false;
+	 if (temperature > - 5 && temperature>30 ||  !isRainy) {
+         System.out.println("Идем гулять");
+
+        }
+    }
+}
